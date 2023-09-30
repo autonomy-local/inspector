77361ee5ba74bc9ac3278980299cb164e9a3e61e
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2023-09-30 23:11:56 +0900

    chore: add url anchor

--- components/AppTabHeader.tsx
+++ components/AppTabHeader.tsx
@@ -18,7 +18,7 @@ export function AppTabHeader() {
       <ul class="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
         <li class="w-full">
           <a
-            href="#"
+            href="#profile"
             class="inline-block w-full p-4 text-gray-900 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white"
             aria-current="page"
           >
@@ -27,7 +27,7 @@ export function AppTabHeader() {
         </li>
         <li class="w-full">
           <a
-            href="#"
+            href="#dashboard"
             class="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
           >
             Dashboard
@@ -35,7 +35,7 @@ export function AppTabHeader() {
         </li>
         <li class="w-full">
           <a
-            href="#"
+            href="#settings"
             class="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
           >
             Settings
@@ -43,7 +43,7 @@ export function AppTabHeader() {
         </li>
         <li class="w-full">
           <a
-            href="#"
+            href="#invoice"
             class="inline-block w-full p-4 bg-white rounded-r-lg hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
           >
             Invoice
